1b1d37dc338153e15cbb217ae794091de9e1362f
강경민/기술개발팀/II <kkang0118@injeinc.co.kr>
2025-12-15 13:51:13 +0900

    feat: 메인페이지 레이아웃 설정

--- section2/src/pages/index.tsx
+++ section2/src/pages/index.tsx
@@ -1,7 +1,10 @@
+import styles from "./index.module.css";
+
 export default function Home() {
   return (
     <>
-      <h1>인덱스</h1>
+      <h1 className={styles.h1}>인덱스</h1>
+      <h2 className={styles.h2}>인덱스</h2>
     </>
   );
 }
